240d1ae03e1223f4cce51cb382dff78122813c3a
rasul <rasul.islyamgali@gmail.com>
2021-07-24 17:24:59 +0300

    finish first working v0.1

--- my_zip_creater.py
+++ my_zip_creater.py
@@ -3,13 +3,28 @@ import time
 
 # в список собираем файлы и папки/каталоги, которых будем архивировать
 # r я ставил, чтобы python не ругалась на слэши, когда будем указывать путь
-my_files = [r'some_file', r'some_file_2', r'some_folder']
+# пример указывания папок и файлов для сохранения
+# OS Windows C:\Users\Admin\Desktop\Programming\test_for_zip
+# OS Windows C:\Users\Admin\Desktop\Programming\test_for_zip_file_2.txt
+# можете указывать сколько угодно файлов и папок
+my_files = [r'',
+            r'']
 
 # указывайте папку, в котором хотите создать zip файл
-folder_for_save = r'some_folder'
+# Пример указвания директории для сохранения zip файла
+# OS Windows C:\Users\Admin\Desktop\Programming\test_folder_to_save_zip
+folder_for_save = r''
 
 # эта строка будет использоваться для zip команды. в конце будут указана название файла.
 # в данном случае оно состоит из времени, в котором zip файл была создана
 # os.sep нужна для указывания разделителя, исходя из ОС.
 name_to_zip_file = folder_for_save + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
+# создаем zip команду
+zip_command = "zip -qr {0} {1}".format(name_to_zip_file, " ".join(my_files))
+
+if __name__ == '__main__':
+    if os.system(zip_command) == 0:
+        print(f'Архивация в zip файл прошла успешно. Ваш файл сохранен по директории:\n{name_to_zip_file}')
+    else:
+        print('Возникли проблемы. Архивация не была выполнена.')
